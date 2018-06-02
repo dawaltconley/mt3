165338d6a3acbc2bc9c9515930368ca308d75a8e
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2018-06-02 04:57:39 -0400

    simplified object fit object creation, consistent with smooth links

--- js/main.js
+++ js/main.js
@@ -114,12 +114,10 @@
  * Object Fit Fallback
  */
 
-    var objectFitElements = toArray(document.querySelectorAll('[class*="object-fit"]'));
-    var objectFitObjects = [];
+    var objectFitElements = [];
 
-    objectFitElements.forEach(function (element) {
-        var newObjectFit = new ObjectFit(element);
-        objectFitObjects.push(newObjectFit);
+    toArray(document.querySelectorAll('[class*="object-fit"]')).forEach(function (element) {
+        objectFitElements.push(new ObjectFit(element));
     });
 
     function ObjectFit(element) {
@@ -135,7 +133,7 @@
     }
 
     function objectFitFallback() {
-        objectFitObjects.forEach(function (object) {
+        objectFitElements.forEach(function (object) {
             object.fallback();
         });
     }
@@ -228,7 +226,7 @@
         forceFullscreenAll();
     }
 
-    if (objectFitObjects.length > 0 && !CSS.supports("object-fit", "cover")) {
+    if (objectFitElements.length > 0 && !CSS.supports("object-fit", "cover")) {
         objectFitFallback();
     }
 
